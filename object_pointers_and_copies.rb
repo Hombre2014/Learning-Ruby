baee2f33d0583ea67537e2a78965d23726695205
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-03-28 17:21:53 +0200

    Object pointers and copies added

---
+++ object_pointers_and_copies.rb
@@ -0,0 +1,37 @@
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+p a.object_id
+p b.object_id
+
+c = [4, 5, 6]
+d = c
+
+p c.object_id == d.object_id
+c.push(7)
+p c
+p d
+
+d = c.dup # Duplicate method
+p c.object_id
+p d.object_id
+c.push(8)
+p c
+p d
+puts
+# Splat arguments
+def sum(*numbers) # Unknown number of arguments
+  sum = 0
+  numbers.each { |num| sum += num }
+  sum
+end
+p sum(1, 2)
+p sum(1, 2, 3, 4)
+puts
+# Any and all methods
+p [1, 3, 5, 7, 2].any? do |number|
+  number.even?
+end
+puts
+p [1, 3, 5, 7].all? { |n| n.odd? }
+p [1, 3, 5, 7, 8].all? { |n| n.odd? }
