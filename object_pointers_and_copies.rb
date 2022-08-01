0548037bb3c1ce30ad75318669c8d232a34f60e4
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-08-01 23:01:11 +0200

    136 Object pointers and object copies

--- object_pointers_and_copies.rb
+++ object_pointers_and_copies.rb
@@ -35,3 +35,20 @@ end
 puts
 p [1, 3, 5, 7].all? { |n| n.odd? }
 p [1, 3, 5, 7, 8].all? { |n| n.odd? }
+
+def custom_join(array, delimiter = "")
+  string = ""
+  last_index = array.length - 1
+  array.each_with_index do |elem, index|
+    string << elem
+    string << delimiter unless index == last_index
+  end
+  string
+end
+
+names = ["Joe", "Moe", "Bob"]
+p custom_join(names)
+p custom_join(names, "&----&")
+
+fact = "Hello"
+p fact.chars
